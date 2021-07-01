978f5de870c537420c5f70b93ba5f5c745133250
quangdz1 <86756645+quangdz1@users.noreply.github.com>
2021-07-01 10:28:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `Trên thế giới có hơn 7 tỷ nụ cười nhưng mà với anh nụ cười em mới là đẹp nhất
+
+Thế nên là làm ơn hãy để anh giúp em giữ nụ cười của em trên môi`,
+    btnIntro: ':333',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không :>> ',
+    btnYes: 'Yêu anh nhất ❤ ',
+    btnNo: 'cứ từ từ anh ạ :>> ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Không yêu cũng phải yêu :3',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Anh Yêu  em nhiều lắm  😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé em yêu :33.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'vâng ạ <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
